9f70364e1893efcaa5fb122c16f2c9e5faa23660
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-01-24 08:35:35 +0700

    build

---
+++ build/precache-manifest.618e9a730f01f63089ae3c54f89c6ca7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1def080766fdcd7ece30eff9bb3ede59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a67ad0137036be9bf8c4",
+    "url": "./static/css/2.11710545.chunk.css"
+  },
+  {
+    "revision": "8af6775904bc28497d4b",
+    "url": "./static/css/main.7ba99cc7.chunk.css"
+  },
+  {
+    "revision": "a67ad0137036be9bf8c4",
+    "url": "./static/js/2.113a5f31.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "./static/js/2.113a5f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43984750877555044589",
+    "url": "./static/js/3.04fc9152.chunk.js"
+  },
+  {
+    "revision": "8076a39f2c6eb4b606ff15bd0342709c",
+    "url": "./static/js/3.04fc9152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af6775904bc28497d4b",
+    "url": "./static/js/main.457d4ecc.chunk.js"
+  },
+  {
+    "revision": "90e517d00f84c16ddc1e",
+    "url": "./static/js/runtime-main.37c5d886.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "./static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "./static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "./static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "./static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "./static/media/primeicons.e01fd413.eot"
+  }
+]);
